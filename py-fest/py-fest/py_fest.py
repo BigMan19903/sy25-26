09add79516245911fa66c4ad2c130d60b34bb87c
max <mgbud302@learner.dvusd.org>
2026-01-12 13:57:53 -0700

    first ver.

--- py-fest/py-fest/py_fest.py
+++ py-fest/py-fest/py_fest.py
@@ -1,1 +1,10 @@
 
+print("n---py-fest 2026 Stage Manager ---")
+print("1. veiw lineup & total Time")
+print("2. ADD a NEW Band")
+print("3. Move First Band to End (Late Arrival)")
+print("4. Remove a Band by Name")
+print("5. Move Band to Specific Position")
+print("6. Exit")
+choice = input("Select an option (1-6): ")
+if choice == "1":print("--- Current Lineup ---")
