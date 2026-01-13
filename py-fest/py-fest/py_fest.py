e114a2c5b73ecbe7f30477cf54293f7cb3663e18
max <mgbud302@learner.dvusd.org>
2026-01-13 13:53:57 -0700

    Tuesday. upt

--- py-fest/py-fest/py_fest.py
+++ py-fest/py-fest/py_fest.py
@@ -1,4 +1,7 @@
 
+from pickle import TRUE
+
+
 print("n---py-fest 2026 Stage Manager ---")
 print("1. veiw lineup & total Time")
 print("2. ADD a NEW Band")
@@ -7,4 +10,5 @@ print("4. Remove a Band by Name")
 print("5. Move Band to Specific Position")
 print("6. Exit")
 choice = input("Select an option (1-6): ")
-if choice == "1":print("--- Current Lineup ---")
+while True:
+if choice = 1 
